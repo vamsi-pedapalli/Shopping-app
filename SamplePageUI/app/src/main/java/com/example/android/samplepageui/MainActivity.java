5c78df7a3a46e9022f68bae58a7d6f17a9568b57
vamsi-pedapalli <vamsipedapalli@gmail.com>
2016-11-17 22:06:40 +0530

    Changes as of 17/11
    Added new activities
    Reformatted UI

--- SamplePageUI/app/src/main/java/com/example/android/samplepageui/MainActivity.java
+++ SamplePageUI/app/src/main/java/com/example/android/samplepageui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.samplepageui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,14 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView rate = (TextView) findViewById(R.id.rate);
+        Button frontPage = (Button) findViewById(R.id.first_page);
 
-        rate.setOnClickListener(new View.OnClickListener() {
+        frontPage.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "clicked rate" , Toast.LENGTH_SHORT).show();
+
+                Intent storeListActivity = new Intent(MainActivity.this, StoreListActivity.class);
+                startActivity(storeListActivity);
+
             }
         });
+
+        Button secondPage = (Button) findViewById(R.id.second);
+        secondPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent profilePage = new Intent(MainActivity.this, StoreProfilePageActivity.class);
+                startActivity(profilePage);
+            }
+        });
+
 
     }
 
---
+++ SamplePageUI/app/src/main/java/com/example/android/samplepageui/StoreListActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.samplepageui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class StoreListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_list);
+
+    }
+}
