8cb55da14aa3b99eda9f467ac1abef2d9214e95d
vamsi-pedapalli <vamsipedapalli@gmail.com>
2016-11-18 20:48:03 +0530

    changes 18/11
    changes as of 18-11

--- SamplePageUI/app/src/main/java/com/example/android/samplepageui/StoreProfilePageActivity.java
+++ SamplePageUI/app/src/main/java/com/example/android/samplepageui/StoreProfilePageActivity.java
@@ -1,7 +1,13 @@
 package com.example.android.samplepageui;
 
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StoreProfilePageActivity extends AppCompatActivity {
 
@@ -9,6 +15,43 @@ public class StoreProfilePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strore_profile_page);
+
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.profile_page_toolbar);
+
+        setSupportActionBar(myChildToolbar);
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("ZARA");
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(null);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share_action:
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "yyy");
+                startActivity(Intent.createChooser(intent, "Share with"));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+        }
 
 
     }
